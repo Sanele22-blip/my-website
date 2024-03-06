4d3592f1a26d91aca23515bf36c6683c086f7a0b
Sanele22-blip <155460774+Sanele22-blip@users.noreply.github.com>
2024-03-06 16:35:39 +0200

    updated JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-alert("Hey");
+function colorMode(){
+    document.body.classList.toggle("dark-mode")
+}
+
+const darkModeToggle = document.querySelector("#darkModeToggle");
+darkModeToggle.addEventListener("click", colorMode);
